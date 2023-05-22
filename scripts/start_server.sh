79da9498974480e2317eaaab796f61a0d01578b6
pranshu-bali97 <105100319+pranshu-bali97@users.noreply.github.com>
2023-05-22 14:58:16 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -2,5 +2,5 @@
 
 set -e
 cd /home/ec2-user/myapp/
-/usr/bin/pm2 reload index.js || /usr/bin/pm2 start
+/usr/bin/pm2 reload index.js 2> /dev/null || /usr/bin/pm2 start
 echo "Code ApplicationStart event script ran."
